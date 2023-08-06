f03ba84f8dd3501fde07ac2018bc3c8e624409db
Elia Menoni <eliamenoni@emenoni.eu>
2023-08-06 18:26:00 +0200

    Component BirthPlace creato

--- PyCDA/Components/BirthPlace.py
+++ PyCDA/Components/BirthPlace.py
@@ -1,13 +1,30 @@
-from ..Core.Exceptions import InvalidGivenValue
 from ..Core import Elements as Element
 from ..Core.Component_Model import Component_Model
 
+from .CS_CodedSimpleValue import CS_CodedSimpleValue
+from .InfrastructureRootTypeId import InfrastructureRootTypeId
+from .II_InstanceIdentifier import II_InstanceIdentifier
+from .Place import Place
+
+
 class BirthPlace(Component_Model):
     """BirthPlace"""
+
     def __init__(self, name: str, data: dict):
-        pass
+        self.name       = name
+        self.realmCode  = Element.Component(CS_CodedSimpleValue, "realmCode", data)
+        self.typeId     = Element.Component(InfrastructureRootTypeId, "typeId", data, as_list=False)
+        self.tempalteId = Element.Component(II_InstanceIdentifier, "tempalteId", data)
+        self.place      = Element.Component(Place, "place", data, required=True, as_list=False)
+        self.classCode  = Element.Attribute("classCode", data, fixed="BIRTHPL")
 
     @classmethod
     def to_dict(cls):
         """to_dict"""
-        return {}
+        return {
+            "realmCode" : CS_CodedSimpleValue.to_dict(),
+            "typeId"    : InfrastructureRootTypeId.to_dict(),
+            "tempalteId": II_InstanceIdentifier.to_dict(),
+            "place"     : Place.to_dict(),
+            "classCode" : "BIRTHPL"
+        }
