36b852bc4b5556538f09b26cb05719f10c656ce0
Elia Menoni <eliamenoni@emenoni.eu>
2023-08-06 17:13:25 +0200

    Componente Person Creato

--- PyCDA/Components/Person.py
+++ PyCDA/Components/Person.py
@@ -2,13 +2,32 @@ from ..Core.Exceptions import InvalidGivenValue
 from ..Core import Elements as Element
 from ..Core.Component_Model import Component_Model
 
+from .CS_CodedSimpleValue import CS_CodedSimpleValue
+from .InfrastructureRootTypeId import InfrastructureRootTypeId
+from .II_InstanceIdentifier import II_InstanceIdentifier
+from .PN_PersonName import PN_PersonName
+
+
 class Person(Component_Model):
     """Person"""
+
     def __init__(self, name: str, data: dict):
-        pass
+        self.name           = name
+        self.realmCode      = Element.Component(CS_CodedSimpleValue, "realmCode", data)
+        self.typeId         = Element.Component(InfrastructureRootTypeId, "typeId", data, as_list=False)
+        self.templateId     = Element.Component(II_InstanceIdentifier, "templateId", data)
+        self._name          = Element.Component(PN_PersonName, "name", data)
+        self.classCode      = Element.Attribute("classCode", data, fixed="PSN")
+        self.determinerCode = Element.Attribute("determinerCode", data, fixed="INSTANCE")
 
     @classmethod
     def to_dict(cls):
         """to_dict"""
-        return {}
-    
+        return {
+            "realmCode"     : CS_CodedSimpleValue.to_dict(),
+            "typeId"        : InfrastructureRootTypeId.to_dict(),
+            "templateId"    : II_InstanceIdentifier.to_dict(),
+            "name"          : PN_PersonName.to_dict(),
+            "classCode"     : "PSN",
+            "determinerCode": "INSTANCE"
+        }
