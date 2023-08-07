b2d1d0b9b8c806f11db0609c1f58798b255c2d19
Elia Menoni <eliamenoni@emenoni.eu>
2023-08-07 13:04:53 +0200

    Component Place creato

--- PyCDA/Components/Place.py
+++ PyCDA/Components/Place.py
@@ -1,14 +1,35 @@
-from ..Core.Exceptions import InvalidGivenValue
 from ..Core import Elements as Element
 from ..Core.Component_Model import Component_Model
 
+from .CS_CodedSimpleValue import CS_CodedSimpleValue
+from .InfrastructureRootTypeId import InfrastructureRootTypeId
+from .II_InstanceIdentifier import II_InstanceIdentifier
+from .EN_GenericName import EN_GenericName
+from .AD_PostalAddress import AD_PostalAddress
+
+
 class Place(Component_Model):
     """Place"""
+
     def __init__(self, name: str, data: dict):
-        pass
+        self.name           = name
+        self.realmCode      = Element.Component(CS_CodedSimpleValue, "realmCode", data)
+        self.typeId         = Element.Component(InfrastructureRootTypeId, "typeId", data, as_list=False)
+        self.templateId     = Element.Component(II_InstanceIdentifier, "templateId", data)
+        self._name          = Element.Component(EN_GenericName, "name", data, as_list=False)
+        self.addr           = Element.Component(AD_PostalAddress, "addr", data, as_list=False)
+        self.classCode      = Element.Attribute("classCode", data, fixed="PLC")
+        self.determinerCode = Element.Attribute("determinerCode", data, fixed="INSTANCE")
 
     @classmethod
     def to_dict(cls):
         """to_dict"""
-        return {}
-    
+        return {
+            "realmCode"     : CS_CodedSimpleValue.to_dict(),
+            "typeId"        : InfrastructureRootTypeId.to_dict(),
+            "templateId"    : II_InstanceIdentifier.to_dict(),
+            "name"          : EN_GenericName.to_dict(),
+            "addr"          : AD_PostalAddress.to_dict(),
+            "classCode"     : "PLC",
+            "determinerCode": "INSTANCE"
+        }
