c0f336ea35f64320dda622082f0dbfda65dd8f9e
Elia Menoni <eliamenoni@emenoni.eu>
2023-08-07 14:05:10 +0200

    Componente Creato

--- PyCDA/Components/AssignedEntity.py
+++ PyCDA/Components/AssignedEntity.py
@@ -1,13 +1,44 @@
-from ..Core.Exceptions import InvalidGivenValue
 from ..Core import Elements as Element
 from ..Core.Component_Model import Component_Model
 
+from .CS_CodedSimpleValue import CS_CodedSimpleValue
+from .InfrastructureRootTypeId import InfrastructureRootTypeId
+from .II_InstanceIdentifier import II_InstanceIdentifier
+from .CE_CodedWithEquivalents import CE_CodedWithEquivalents
+from .AD_PostalAddress import AD_PostalAddress
+from .TEL_TelecomincationAddress import TEL_TelecomincationAddress
+from .Person import Person
+from .Organization import Organization
+
+
 class AssignedEntity(Component_Model):
     """AssignedEntity"""
+
     def __init__(self, name: str, data: dict):
-        pass
+        self.name                    = name
+        self.realmCode               = Element.Component(CS_CodedSimpleValue, "realmCode", data)
+        self.typeId                  = Element.Component(InfrastructureRootTypeId, "typeId", data, as_list=False)
+        self.templateId              = Element.Component(II_InstanceIdentifier, "templateId", data)
+        self.id                      = Element.Component(II_InstanceIdentifier, "id", data, required=True)
+        self.code                    = Element.Component(CE_CodedWithEquivalents, "code", data, as_list=False)
+        self.addr                    = Element.Component(AD_PostalAddress, "addt", data)
+        self.telecom                 = Element.Component(TEL_TelecomincationAddress, "telecom", data)
+        self.assignedPerson          = Element.Component(Person, "assignedPerson", data, as_list=False)
+        self.representedOrganization = Element.Component(Organization, "representedOrganization", data, as_list=False)
+        self.classCode               = Element.Attribute("classCode", data, fixed="ASSIGNED")
 
     @classmethod
     def to_dict(cls):
-        """to_dict"""        
-        return {}
+        """to_dict"""
+        return {
+            "realmCode"              : CS_CodedSimpleValue.to_dict(),
+            "typeId"                 : InfrastructureRootTypeId.to_dict(),
+            "templateId"             : II_InstanceIdentifier.to_dict(),
+            "id"                     : II_InstanceIdentifier.to_dict(),
+            "code"                   : CE_CodedWithEquivalents.to_dict(),
+            "addr"                   : AD_PostalAddress.to_dict(),
+            "telecom"                : TEL_TelecomincationAddress.to_dict(),
+            "assignedPerson"         : Person.to_dict(),
+            "representedOrganization": Organization.to_dict(),
+            "classCode"              : "ASSIGNED"
+        }
