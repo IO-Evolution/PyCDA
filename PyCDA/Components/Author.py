c153b94aef31ebfae969ae0bacd7d826b33a94b9
Elia Menoni <eliamenoni@emenoni.eu>
2023-08-07 13:18:44 +0200

    Componente Author Creato

--- PyCDA/Components/Author.py
+++ PyCDA/Components/Author.py
@@ -2,12 +2,38 @@ from ..Core.Exceptions import InvalidGivenValue
 from ..Core import Elements as Element
 from ..Core.Component_Model import Component_Model
 
+from .CS_CodedSimpleValue import CS_CodedSimpleValue
+from .InfrastructureRootTypeId import InfrastructureRootTypeId
+from .II_InstanceIdentifier import II_InstanceIdentifier
+from .CE_CodedWithEquivalents import CE_CodedWithEquivalents
+from .TS_PointInTime import TS_PointInTime
+from .AssignedAuthor import AssignedAuthor
+
+
 class Author(Component_Model):
     """Author"""
+
     def __init__(self, name: str, data: dict):
-        pass
+        self.name               = name
+        self.realmCode          = Element.Component(CS_CodedSimpleValue, "realmCode", data)
+        self.typeId             = Element.Component(InfrastructureRootTypeId, "typeId", data, as_list=False)
+        self.templateId         = Element.Component(II_InstanceIdentifier, "templateId", data)
+        self.functionCode       = Element.Component(CE_CodedWithEquivalents, "functionCode", data, as_list=False)
+        self.time               = Element.Component(TS_PointInTime, "time", data, required=True, as_list=False)
+        self.assignedAuthor     = Element.Component(AssignedAuthor, "assignedAuthor", data, required=True, as_list=False)
+        self.contextControlCode = Element.Attribute("contectControlCode", data, fixed="OP")
+        self.typeCode           = Element.Attribute("typeCode", data, fixed="AUT")
 
     @classmethod
     def to_dict(cls):
-        """to_dict"""        
-        return {}
+        """to_dict"""
+        return {
+            "realmCode"         : CS_CodedSimpleValue.to_dict(),
+            "typeId"            : InfrastructureRootTypeId.to_dict(),
+            "templateId"        : II_InstanceIdentifier.to_dict(),
+            "functionCode"      : CE_CodedWithEquivalents.to_dict(),
+            "time"              : TS_PointInTime.to_dict(),
+            "assignedAuthor"    : AssignedAuthor.to_dict(),
+            "contectControlCode": "OP",
+            "typeCode"          : "AUT"
+        }
