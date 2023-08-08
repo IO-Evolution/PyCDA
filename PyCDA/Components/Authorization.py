169447c2e874bda57af113cf970adeba67b6f43c
Elia Menoni <eliamenoni@emenoni.eu>
2023-08-09 00:35:15 +0200

    Modifiche di formattazione

--- PyCDA/Components/Authorization.py
+++ PyCDA/Components/Authorization.py
@@ -12,12 +12,12 @@ class Authorization(Component_Model):
     """Authorization"""
 
     def __init__(self, name: str, data: dict):
-        self.name = name
-        self.realmCode = Element.Component(CS_CodedSimpleValue, "realmCode", data)
-        self.typeId = Element.Component(InfrastructureRootTypeId, "typeId", data, as_list=False)
+        self.name       = name
+        self.realmCode  = Element.Component(CS_CodedSimpleValue, "realmCode", data)
+        self.typeId     = Element.Component(InfrastructureRootTypeId, "typeId", data, as_list=False)
         self.templateId = Element.Component(II_InstanceIdentifier, "templateId", data)
-        self.consent = Element.Component(Consent, "consent", data, required=True, as_list=False)
-        self.typeCode = Element.Attribute("typeCode", data, fixed="AUTH")
+        self.consent    = Element.Component(Consent, "consent", data, required=True, as_list=False)
+        self.typeCode   = Element.Attribute("typeCode", data, fixed="AUTH")
 
     @classmethod
     def to_dict(cls):
