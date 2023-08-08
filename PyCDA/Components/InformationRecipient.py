3de6ce4e12fa091eff74844fdca8f772516be8b6
Elia Menoni <eliamenoni@emenoni.eu>
2023-08-08 17:15:56 +0200

    Componente creato

--- PyCDA/Components/InformationRecipient.py
+++ PyCDA/Components/InformationRecipient.py
@@ -2,13 +2,23 @@ from ..Core.Exceptions import InvalidGivenValue
 from ..Core import Elements as Element
 from ..Core.Component_Model import Component_Model
 
+from .CS_CodedSimpleValue import CS_CodedSimpleValue
+from .InfrastructureRootTypeId import InfrastructureRootTypeId
+from .II_InstanceIdentifier import II_InstanceIdentifier
+from .IntendedRecipient import IntendedRecipient
+
 class InformationRecipient(Component_Model):
     """InformationRecipient"""
+
     def __init__(self, name: str, data: dict):
-        pass
+        self.name = name
+        self.realmCode=Element.Component(CS_CodedSimpleValue, "realmCode", data)
+        self.typeId=Element.Component(InfrastructureRootTypeId, "typeId", data, as_list=False)
+        self.templateId=Element.Component(II_InstanceIdentifier, "templateId", data)
+        self.intendedRecipient=Element.Component(IntendedRecipient, "intendedRecipient", data, required=True, as_list=False)
+        self.typeCode=Element.Attribute("typeCode", data, fixed="PRC")
 
     @classmethod
     def to_dict(cls):
         """to_dict"""
         return {}
-    
