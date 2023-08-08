141d33f5d0dd2b19da00908260a4a348a4bb437e
Elia Menoni <eliamenoni@emenoni.eu>
2023-08-09 01:03:23 +0200

    Componente creato

--- PyCDA/Components/Component4.py
+++ PyCDA/Components/Component4.py
@@ -1,20 +1,69 @@
-from ..Core.Exceptions import InvalidGivenValue
 from ..Core import Elements as Element
 from ..Core.Component_Model import Component_Model
+
+from .CS_CodedSimpleValue import CS_CodedSimpleValue
+from .InfrastructureRootTypeId import InfrastructureRootTypeId
+from .II_InstanceIdentifier import II_InstanceIdentifier
+from .INT_IntegerNumber import INT_IntegerNumber
+from .BL_Boolean import BL_Boolean
+from .Act import Act
+from .Encounter import Encounter
+from .Observation import Observation
+from .ObservationMedia import ObservationMedia
+from .Organizer import Organizer
+from .Procedure import Procedure
+from .RegionOfInterest import RegionOfInterest
+from .SubstanceAdministration import SubstanceAdministration
+from .Supply import Supply
 
 
 class Component4(Component_Model):
     """Component4"""
 
     def __init__(self, name: str, data: dict):
-        pass
+        self.name                    = name
+        self.realmCode               = Element.Component(CS_CodedSimpleValue, "realmCode", data)
+        self.typeId                  = Element.Component(InfrastructureRootTypeId, "typeId", data, as_list=False)
+        self.templateId              = Element.Component(II_InstanceIdentifier, "templateId", data)
+        self.sequenceNumber          = Element.Component(INT_IntegerNumber, "sequenceNumber", data, as_list=False)
+        self.seperatableInd          = Element.Component(BL_Boolean, "seperatableInd", data, as_list=False)
+        self.act                     = Element.Component(Act, "act", data, as_list=False)
+        self.encounter               = Element.Component(Encounter, "encounter", data, as_list=False)
+        self.observation             = Element.Component(Observation, "observation", data, as_list=False)
+        self.observationMedia        = Element.Component(ObservationMedia, "observationMedia", data, as_list=False)
+        self.organizer               = Element.Component(Organizer, "organizer", data, as_list=False)
+        self.procedure               = Element.Component(Procedure, "procedure", data, as_list=False)
+        self.regionOfInterest        = Element.Component(RegionOfInterest, "regionOfInterest", data, as_list=False)
+        self.substanceAdministration = Element.Component(SubstanceAdministration, "substanceAdministration", data, as_list=False)
+        self.supply                  = Element.Component(Supply, "supply", data, as_list=False)
+        self.contextConductionInd    = Element.Attribute("contextConductionInd", data, fixed="true")
+        self.typeCode                = Element.Attribute("typeCode", data, fixed="COMP")
 
     @classmethod
     def to_dict(cls):
         """to_dict"""
-        return {}
+        return {
+            "realmCode"              : CS_CodedSimpleValue.to_dict(),
+            "typeId"                 : InfrastructureRootTypeId.to_dict(),
+            "templateId"             : II_InstanceIdentifier.to_dict(),
+            "sequenceNumber"         : INT_IntegerNumber.to_dict(),
+            "act"                    : BL_Boolean.to_dict(),
+            "encounter"              : Encounter.to_dict(),
+            "observation"            : Observation.to_dict(),
+            "observationMedia"       : ObservationMedia.to_dict(),
+            "organizer"              : Organizer.to_dict(),
+            "procedure"              : Procedure.to_dict(),
+            "regionOfInterest"       : RegionOfInterest.to_dict(),
+            "substanceAdministration": SubstanceAdministration.to_dict(),
+            "supply"                 : Supply.to_dict(),
+            "contextConductionInd"   : "true",
+            "typeCode"               : "COMP"
+        }
 
     @classmethod
     def to_dict_req(cls):
         """to_dict"""
-        return {}
+        return {
+            "contextConductionInd": "true",
+            "typeCode"            : "COMP"
+        }
